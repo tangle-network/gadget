e06fdde2a01f58241821af1ca4d9e8ee2461dc71
Shady Khalifa <dev+github@shadykhalifa.me>
2023-11-17 22:00:08 +0200

    Add README on how to run the playground

---
+++ playground/generate_certs.sh
@@ -0,0 +1,12 @@
+# A simple script that invokes a simple binary to generate a self-signed
+# certificate for use in the playground.
+
+# 1. Build the binary
+cargo build --bin certgen
+# 2. Run the binary
+# Generate 8 certs from 0 to 7
+for i in {0..7}
+  do
+      ./target/debug/certgen --i $i
+  done
+echo "Done generating certs"
