02c9453711568bbd9a67b0c54b7e166d9ce90fb6
Thomas Braun <braunth@oregonstate.edu>
2024-02-07 08:29:42 -0500

    switch to nightly channel, fmt

--- test-utils/src/mock.rs
+++ test-utils/src/mock.rs
@@ -22,7 +22,10 @@ use frame_support::{
     PalletId,
 };
 use frame_system::EnsureSigned;
-use gadget_common::client::{AccountId, MaxActiveJobsPerValidator, MaxDataLen, MaxKeyLen, MaxParticipants, MaxProofLen, MaxSignatureLen, MaxSubmissionLen};
+use gadget_common::client::{
+    AccountId, MaxActiveJobsPerValidator, MaxDataLen, MaxKeyLen, MaxParticipants, MaxProofLen,
+    MaxSignatureLen, MaxSubmissionLen,
+};
 use pallet_jobs_rpc_runtime_api::BlockNumberOf;
 use sc_client_api::{FinalityNotification, FinalizeSummary};
 use sc_utils::mpsc::{tracing_unbounded, TracingUnboundedReceiver, TracingUnboundedSender};
