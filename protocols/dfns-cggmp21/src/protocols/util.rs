6c5d90061abab52c9f4a936f37f44b2ab7fbd3ef
Thomas Braun <braunth@oregonstate.edu>
2024-03-01 15:02:32 -0500

    revert util.rs

--- protocols/dfns-cggmp21/src/protocols/util.rs
+++ protocols/dfns-cggmp21/src/protocols/util.rs
@@ -1,5 +1,6 @@
 use gadget_common::gadget::message::UserID;
-use itertools::Itertools;
+use rand::prelude::SliceRandom;
+use sp_core::ecdsa;
 use sp_core::ecdsa::Public;
 use std::collections::HashMap;
 
@@ -16,47 +17,44 @@ pub type ChosenSigners = (u16, Vec<u16>, HashMap<UserID, Public>);
 /// # Panics
 /// If the current participant is not in the list of participants it will panic.
 pub fn choose_signers<R: rand::Rng>(
-    _rng: &mut R,
-    my_account_id: &Public,
-    participants: &[Public],
+    rng: &mut R,
+    my_role_key: &ecdsa::Public,
+    participants: &[ecdsa::Public],
     t: u16,
 ) -> Result<ChosenSigners, gadget_common::Error> {
     let selected_participants = participants
-        .iter()
-        .take(t as usize)
-        .copied()
-        .enumerate()
-        .map(|(i, p)| (i as UserID, p))
-        .sorted_by_key(|k| k.0)
-        .collect::<HashMap<_, _>>();
+        .choose_multiple(rng, t as usize)
+        .cloned()
+        .collect::<Vec<_>>();
 
     let selected_participants_indices = selected_participants
         .iter()
-        .map(|p| participants.iter().position(|x| x == p.1).unwrap() as u16)
-        .sorted()
+        .map(|p| participants.iter().position(|x| x == p).unwrap() as u16)
         .collect::<Vec<_>>();
 
-    // Generate a new mapping of part indexes starting from 0 and incrementing to t (e.g., [0, 1, 2, ...])
-    let _user_id_to_account_id_mapping = selected_participants_indices
+    let j = participants
         .iter()
-        .map(|idx| participants[*idx as usize])
+        .position(|p| p == my_role_key)
+        .expect("Should exist") as u16;
+
+    let i = selected_participants_indices
+        .iter()
+        .position(|p| p == &j)
+        .map(|i| i as u16)
+        .ok_or_else(|| gadget_common::Error::ParticipantNotSelected {
+            id: *my_role_key,
+            reason: String::from("we are not selected to sign"),
+        })?;
+
+    let user_id_to_account_id_mapping = selected_participants
+        .clone()
+        .into_iter()
         .enumerate()
         .map(|(i, p)| (i as UserID, p))
-        .collect::<HashMap<UserID, Public>>();
-
-    // Find our position in the NEW mapping
-    let my_position = *selected_participants
-        .iter()
-        .find(|(_id, pk)| pk == &my_account_id)
-        .ok_or_else(|| gadget_common::Error::ParticipantNotSelected {
-            id: *my_account_id,
-            reason: "We are not signing this round".to_string(),
-        })?
-        .0;
-
+        .collect();
     Ok((
-        my_position as u16,
+        i,
         selected_participants_indices,
-        selected_participants,
+        user_id_to_account_id_mapping,
     ))
 }
