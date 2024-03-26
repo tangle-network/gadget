ed1b318eccd186f4703c1d5b9085ca3abe4b55fa
shekohex <dev+github@shadykhalifa.me>
2024-03-26 13:14:29 +0200

    feat: Support more gadgets in the shell (#117)

---
+++ protocols/bls/src/constants.rs
@@ -0,0 +1,4 @@
+pub const GENNARO_DKG_BLS_381_KEYGEN_PROTOCOL_NAME: &str =
+    "/tangle/gennaro-dkg-bls-381/keygen/1.0.0";
+pub const GENNARO_DKG_BLS_381_SIGNING_PROTOCOL_NAME: &str =
+    "/tangle/gennaro-dkg-bls-381/signing/1.0.0";
