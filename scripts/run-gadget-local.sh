da46cf0a5a2d8354018bcff745d847ec98e1f0a4
Salman Pathan <pathansalman555@gmail.com>
2024-05-13 20:13:44 +0530

    Add script to run gadget and update readme (#143)

---
+++ scripts/run-gadget-local.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+# ensure we kill all child processes when we exit
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+
+echo "*** Start Gadget ***"
+# Stage gadget for Alice
+./target/release/shell-manager \
+  --protocols-config ./global_protocols.toml \
+  --shell-config shell-configs/local-testnet-0.toml &
+
+# Stage gadget for Bob
+./target/release/shell-manager \
+  --protocols-config ./global_protocols.toml \
+  --shell-config shell-configs/local-testnet-1.toml &
+
+# Stage gadget for Charlie
+./target/release/shell-manager \
+  --protocols-config ./global_protocols.toml \
+  --shell-config shell-configs/local-testnet-2.toml \
+  -vvv
+
+popd
