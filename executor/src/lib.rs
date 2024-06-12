147886b4374b91e0fa50fc104b9bc6a11ad15833
Tjemmmic <mdtjemmes@proton.me>
2024-06-12 17:27:04 -0500

    More executor functionalities

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -64,14 +64,24 @@ pub async fn run_executor(instructions: &str) {
 pub async fn run_tangle_validator() -> Result<(), Box<dyn Error>> {
     let mut manager = GadgetProcessManager::new();
 
-    // let install = manager.run(&*"binary_install", &*"wget https://github.com/webb-tools/tangle/releases/download/v1.0.0/tangle-default-linux-amd64".to_string()).await?;
-    // let make_executable = manager.run(&*"make_executable", &*"chmod +x tangle-default-linux-amd64".to_string()).await?;
+    let install = manager.run("binary_install".to_string(), &*"wget https://github.com/webb-tools/tangle/releases/download/v1.0.0/tangle-default-linux-amd64".to_string()).await?;
+    manager.focus_service_to_completion(install).await?;
 
-    // let start_validator = manager.run("tangle_validator".to_string(), "./tangle-default-linux-amd64").await?;
-    let ping_test = manager
-        .run("ping_test".to_string(), "ping -c 5 localhost")
+    let make_executable = manager
+        .run(
+            "make_executable".to_string(),
+            &*"chmod +x tangle-default-linux-amd64".to_string(),
+        )
         .await?;
-    manager.focus_service_to_completion(ping_test).await?;
+    manager.focus_service_to_completion(make_executable).await?;
+
+    let start_validator = manager
+        .run(
+            "tangle_validator".to_string(),
+            "./tangle-default-linux-amd64",
+        )
+        .await?;
+    manager.focus_service_to_completion(start_validator).await?;
 
     Ok(())
 }
