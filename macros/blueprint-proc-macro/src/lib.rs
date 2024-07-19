1fe145685ff4c46acae74107fd991c98b2627dea
Shady Khalifa <dev+github@shadykhalifa.me>
2024-07-19 19:59:20 +0300

    add docs to the job macro

--- macros/blueprint-proc-macro/src/lib.rs
+++ macros/blueprint-proc-macro/src/lib.rs
@@ -24,26 +24,39 @@ mod blueprint;
 mod job;
 
 /// A procedural macro that annotates a function as a job.
-/// It generates a struct with the same name as the function (in `PascalCase`)
-/// and with a postfix of `Job` that holds the function's arguments (passed in `params`)
-/// and the function result (passed in `result`).
 ///
 /// # Example
-/// ```rust,ignore
-/// # use blueprint_macro::job;
-/// #[job(params(n, t), result(Bytes))]
-/// fn keygen(n: u16, t: u8) -> Bytes {
-/// // ...
+/// ```rust,no_run
+/// # use gadget_blueprint_proc_macro::job;
+///
+/// /// A simple add function that adds two numbers.
+/// #[job(id = 0, params(a, b), result(u32))]
+/// pub fn add(a: u32, b: u32) -> u32 {
+///    a + b
 /// }
 /// ```
-/// This will generate the following struct:
-/// ```rust,ignore
-/// #[derive(Debug, Serialize, Deserialize)]
-/// struct KeygenJob {
-///     params: (u16, u8),
-///     result: Bytes
+///
+/// The `job` macro generates the following code:
+/// ```rust
+/// /// A Job Definition ID for the [`add`] function.
+/// #[automatically_derived]
+/// pub const ADD_JOB_ID: u8 = 0;
+///
+/// /// A Job Definition for the [`add`] function.
+/// #[automatically_derived]
+/// pub const ADD_JOB_DEF: &str = r#"{"metadata":{"name":"add","description":"A simple add function that adds two numbers"},"params":["Uint32", "Uint32"],"result":["Uint32"],"verifier":"None"}"#;
+///
+/// /// A simple add function that adds two numbers.
+/// pub fn add(a: u32, b: u32) -> u32 {
+///   a + b
 /// }
 /// ```
+///
+/// # Parameters
+/// - `id`: The unique identifier for the job (must be in the range of 0..[`u8::MAX`])
+/// - `params`: The parameters of the job function, must be a tuple of identifiers in the function signature.
+/// - `result`: The result of the job function, must be a type that this job returns.
+///    also, it can be omitted if the return type is simple to infer, like `u32` or `Vec<u8>` just use `_`.
 #[proc_macro_attribute]
 pub fn job(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as job::JobArgs);
