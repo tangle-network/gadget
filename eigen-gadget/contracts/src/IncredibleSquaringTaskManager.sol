489ea0309bbe427a811fba61e68f255e5e8cb580
Drew Stone <drewstone329@gmail.com>
2024-07-22 16:06:31 -0600

    Update eigen contracts and submodules

--- eigen-gadget/contracts/src/IncredibleSquaringTaskManager.sol
+++ eigen-gadget/contracts/src/IncredibleSquaringTaskManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
