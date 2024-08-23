2aa8c09072f0004d6d4d67e607b825fefa248710
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-08-23 19:59:04 -0400

    gadget-sdk: Fix clippy::missing_errors_doc

--- sdk/src/keystore/sr25519.rs
+++ sdk/src/keystore/sr25519.rs
@@ -8,6 +8,12 @@ pub use schnorrkel::Signature;
 /// The context used for signing.
 const SIGNING_CTX: &[u8] = b"substrate";
 
+/// Generate a new secret key.
+///
+/// # Errors
+///
+/// * `seed` is not the correct length (64).
+/// * `seed` contains an invalid scalar.
 pub fn generate_with_optional_seed(
     seed: Option<&[u8]>,
 ) -> Result<Secret, schnorrkel::SignatureError> {
@@ -19,11 +25,24 @@ pub fn generate_with_optional_seed(
     }
 }
 
+/// Sign a message with the given secret key.
+///
+/// # Errors
+///
+/// Returns an error if the signature fails to be verified.
 pub fn sign(secret: &Secret, msg: &[u8]) -> Result<Signature, schnorrkel::SignatureError> {
     let public = secret.to_public();
     secret.sign_simple_doublecheck(SIGNING_CTX, msg, &public)
 }
 
+/// Create a secret key from a byte slice.
+///
+/// If the slice is 32 bytes long, a new random nonce is added to the secret key.
+///
+/// # Errors
+///
+/// * `bytes` is not the correct length (32 or 64).
+/// * `bytes` contains an invalid scalar.
 pub fn secret_from_bytes(bytes: &[u8]) -> Result<Secret, schnorrkel::SignatureError> {
     if bytes.len() == 32 {
         // add a new random nonce to the secret key
