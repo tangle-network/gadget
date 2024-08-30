8add1f6601cced3fbfc874297b73b46b762fbf1a
Drew Stone <drewstone329@gmail.com>
2024-08-31 00:48:01 +0900

    fix: move eigenlayer work out while its not building

--- blueprints/incredible-squaring/src/main.rs
+++ blueprints/incredible-squaring/src/main.rs
@@ -98,108 +98,6 @@ impl GadgetRunner for TangleGadgetRunner {
     }
 }
 
-struct EigenlayerGadgetRunner<R: lock_api::RawRwLock> {
-    env: GadgetConfiguration<R>,
-}
-
-struct EigenlayerEventWatcher<T> {
-    _phantom: std::marker::PhantomData<T>,
-}
-
-impl<T: Config> EventWatcher<T> for EigenlayerEventWatcher<T> {
-    const TAG: &'static str = "eigenlayer";
-    type Contract =
-        IncredibleSquaringTaskManager::IncredibleSquaringTaskManagerInstance<T::T, T::P, T::N>;
-    type Event = IncredibleSquaringTaskManager::NewTaskCreated;
-    const GENESIS_TX_HASH: FixedBytes<32> = FixedBytes::from_slice(&[0; 32]);
-}
-
-#[async_trait::async_trait]
-impl GadgetRunner for EigenlayerGadgetRunner<parking_lot::RawRwLock> {
-    async fn register(&self) -> Result<()> {
-        if self.env.test_mode {
-            self.env.logger.info("Skipping registration in test mode");
-            return Ok(());
-        }
-
-        // TODO: Register to become an operator
-        let keystore = self.env.keystore().map_err(|e| eyre!(e))?;
-        let ecdsa_key = self.env.first_ecdsa_signer().map_err(|e| eyre!(e))?;
-        //let signer: PrivateKeySigner = PrivateKeySigner::from_signing_key(signing_key);
-        // Implement Eigenlayer-specific registration logic here
-        // For example:
-        // let contract = YourEigenlayerContract::new(contract_address, provider);
-        // contract.register_operator(signer).await?;
-
-        tracing::info!("Registered operator for Eigenlayer");
-        Ok(())
-    }
-
-    async fn run(&self) -> Result<()> {
-        let env = gadget_sdk::env::load(Some(Protocol::Eigenlayer)).map_err(|e| eyre!(e))?;
-        let keystore = env.keystore().map_err(|e| eyre!(e))?;
-        // get the first ECDSA key from the keystore and register with it.
-        let ecdsa_key = self.env.first_ecdsa_signer().map_err(|e| eyre!(e))?;
-        let ed_key = keystore
-            .iter_ed25519()
-            .next()
-            .ok_or_eyre("Unable to find ED25519 key")?;
-        let ed_public_bytes = ed_key.as_ref(); // 32 byte len
-
-        let ed_public = ed25519_zebra::VerificationKey::try_from(ed_public_bytes)
-            .map_err(|e| eyre!("Unable to create ed25519 public key"))?;
-
-        let signing_key = SigningKey::from(ecdsa_key.public_key());
-        let priv_key_signer: PrivateKeySigner = PrivateKeySigner::from_signing_key(signing_key);
-        let wallet = EthereumWallet::from(priv_key_signer.clone());
-        // Set up eignelayer AVS
-        let contract_address = Address::from_slice(&[0; 20]);
-        // Set up the HTTP provider with the `reqwest` crate.
-        let provider = ProviderBuilder::new()
-            .with_recommended_fillers()
-            .wallet(wallet)
-            .on_http(env.rpc_endpoint.parse()?);
-
-        let sr_secret = keystore
-            .expose_ed25519_secret(&ed_public)
-            .map_err(|e| eyre!(e))?
-            .ok_or_eyre("Unable to find ED25519 secret")?;
-        let mut sr_secret_bytes = sr_secret.as_ref().to_vec(); // 64 byte len
-
-        let identity = libp2p::identity::Keypair::ed25519_from_bytes(&mut sr_secret_bytes)
-            .map_err(|e| eyre!("Unable to construct libp2p keypair: {e:?}"))?;
-
-        // TODO: Fill in and find the correct values for the network configuration
-        // TODO: Implementations for reading set of operators from Tangle & Eigenlayer
-        let network_config: NetworkConfig = NetworkConfig {
-            identity,
-            ecdsa_key,
-            bootnodes: vec![],
-            bind_ip: self.env.bind_addr,
-            bind_port: self.env.bind_port,
-            topics: vec!["__TESTING_INCREDIBLE_SQUARING".to_string()],
-            logger: self.env.logger.clone(),
-        };
-
-        let network: GossipHandle = start_p2p_network(network_config).map_err(|e| eyre!(e))?;
-        // let x_square_eigen = blueprint::XsquareEigenEventHandler {
-        //     ctx: blueprint::MyContext { network, keystore },
-        // };
-        //
-        // let contract: IncredibleSquaringTaskManager::IncredibleSquaringTaskManagerInstance = IncredibleSquaringTaskManager::IncredibleSquaringTaskManagerInstance::new(contract_address, provider);
-        //
-        // EventWatcher::run(
-        //     &EigenlayerEventWatcher,
-        //     contract,
-        //     // Add more handler here if we have more functions.
-        //     vec![Box::new(x_square_eigen)],
-        // )
-        // .await?;
-
-        Ok(())
-    }
-}
-
 fn create_gadget_runner(
     protocol: Protocol,
 ) -> (
@@ -209,7 +107,7 @@ fn create_gadget_runner(
     let env = gadget_sdk::env::load(Some(protocol)).expect("Failed to load environment");
     match protocol {
         Protocol::Tangle => (env.clone(), Arc::new(TangleGadgetRunner { env })),
-        Protocol::Eigenlayer => (env.clone(), Arc::new(EigenlayerGadgetRunner { env })),
+        Protocol::Eigenlayer => panic!("Eigenlayer not implemented yet"),
     }
 }
 
