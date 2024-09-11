a58780ed3f7128f25c003a86bc2e0005fe049846
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-09-11 15:57:12 -0400

    sdk: Document `Client` trait

--- sdk/src/clients/mod.rs
+++ sdk/src/clients/mod.rs
@@ -7,6 +7,10 @@ pub mod tangle;
 #[async_trait]
 #[auto_impl(Arc)]
 pub trait Client<Event>: Clone + Send + Sync {
+    /// Fetch the next event from the client.
     async fn next_event(&self) -> Option<Event>;
+    /// Fetch the latest event from the client.
+    ///
+    /// If no event has yet been fetched, the client will call [`next_event`](Self::next_event).
     async fn latest_event(&self) -> Option<Event>;
 }
