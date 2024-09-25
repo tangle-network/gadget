17abccd1787769ff13b6e64fd2bb62e37b72fe52
Thomas Braun <tbraun96@gmail.com>
2024-09-25 12:30:30 -0400

    test repassing

--- blueprint-manager/src/sdk/utils.rs
+++ blueprint-manager/src/sdk/utils.rs
@@ -60,13 +60,6 @@ pub fn generate_process_arguments(
         format!("--bind-addr={}", gadget_config.bind_addr),
         format!("--bind-port={}", gadget_config.bind_port),
         format!("--url={}", gadget_config.url),
-        format!(
-            "--logger=Blueprint-{blueprint_id}-Service-{service_id}-{}",
-            opt.instance_id.clone().unwrap_or_else(|| format!(
-                "{}-{}",
-                gadget_config.bind_addr, gadget_config.bind_port
-            ))
-        ),
         format!("--keystore-uri={}", gadget_config.keystore_uri),
         format!("--chain={}", gadget_config.chain),
         format!("--verbose={}", opt.verbose),
