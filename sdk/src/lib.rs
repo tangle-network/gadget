d3c0bd2f3af82ba7786eda284a443df1b8bae42e
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-10-17 21:40:32 -0400

    feat(sdk): re-export `libp2p`

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -72,6 +72,7 @@ pub use alloy_rpc_types;
 pub use error::Error;
 pub use futures;
 pub use gadget_blueprint_proc_macro::*;
+pub use libp2p;
 pub use parking_lot;
 pub use structopt;
 pub use subxt_core;
