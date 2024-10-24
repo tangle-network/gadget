53d2d9926a439ffe95318841e8093801790b8f95
Thomas Braun <tbraun96@gmail.com>
2024-10-23 20:36:07 -0400

    Cleanup

--- macros/blueprint-proc-macro/src/event_listener/tangle.rs
+++ macros/blueprint-proc-macro/src/event_listener/tangle.rs
@@ -14,35 +14,4 @@ pub(crate) fn generate_tangle_event_handler(
         #[automatically_derived]
         impl gadget_sdk::event_listener::markers::IsTangle for #struct_name {}
     }
-    /*
-           #[automatically_derived]
-       #[async_trait::async_trait]
-       impl gadget_sdk::events_watcher::substrate::EventHandler<gadget_sdk::clients::tangle::runtime::TangleConfig, gadget_sdk::tangle_subxt::tangle_testnet_runtime::api::services::events::JobCalled> for #struct_name {
-           async fn handle(&self, event: &gadget_sdk::tangle_subxt::tangle_testnet_runtime::api::services::events::JobCalled) -> Result<Vec<gadget_sdk::tangle_subxt::tangle_testnet_runtime::api::runtime_types::tangle_primitives::services::field::Field<gadget_sdk::subxt_core::utils::AccountId32>>, gadget_sdk::events_watcher::Error> {
-               use gadget_sdk::tangle_subxt::tangle_testnet_runtime::api::runtime_types::tangle_primitives::services::field::Field;
-               let mut args_iter = event.args.clone().into_iter();
-               #(#params_tokens)*
-               #fn_call
-
-               let mut result = Vec::new();
-               #(#result_tokens)*
-
-               Ok(result)
-           }
-
-           /// Returns the job ID
-           fn job_id(&self) -> u8 {
-               #job_id
-           }
-
-           /// Returns the service ID
-           fn service_id(&self) -> u64 {
-               self.service_id
-           }
-
-           fn signer(&self) -> &gadget_sdk::keystore::TanglePairSigner<gadget_sdk::ext::sp_core::sr25519::Pair> {
-               &self.signer
-           }
-       }
-    */
 }
