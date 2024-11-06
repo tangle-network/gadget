a3cce07f8bc0875c063c20b73772d0ff959cd063
Drew Stone <drewstone329@gmail.com>
2024-11-06 12:30:54 +0700

    chore: attempt fix

--- blueprints/examples/contracts/src/Counter.sol
+++ blueprints/examples/contracts/src/Counter.sol
@@ -2,13 +2,27 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    struct NumberSequence {
+        uint256[5] numbers;
     }
 
-    function increment() public {
-        number++;
+    mapping(bytes32 => NumberSequence) sequences;
+
+    event NumberIncremented(bytes32 indexed id, uint8 indexed index, uint256 indexed oldValue, uint256 newValue);
+
+    function setNumber(bytes32 id, uint8 index, uint256 newNumber) public {
+        require(index < 5, "Index out of bounds");
+        sequences[id].numbers[index] = newNumber;
+    }
+
+    function increment(bytes32 id, uint8 index) public {
+        require(index < 5, "Index out of bounds");
+        uint256 oldValue = sequences[id].numbers[index];
+        sequences[id].numbers[index]++;
+        emit NumberIncremented(id, index, oldValue, sequences[id].numbers[index]);
+    }
+
+    function getSequence(bytes32 id) public view returns (uint256[5] memory) {
+        return sequences[id].numbers;
     }
 }
--- blueprints/examples/src/examples/mod.rs
+++ blueprints/examples/src/examples/mod.rs
@@ -1,3 +1,2 @@
 pub mod periodic_web_poller;
 pub mod raw_tangle_events;
-pub mod sequential_event_listener;
