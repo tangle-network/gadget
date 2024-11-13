41f2ed7b415ab08ca1f570372ce888de1d3b81f4
Tjemmmic <mdtjemmes@proton.me>
2024-11-13 16:40:32 -0600

    fix(ci): workflow fix after merge

--- blueprints/examples/build.rs
+++ blueprints/examples/build.rs
@@ -2,10 +2,5 @@ fn main() {
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/main.rs");
     blueprint_metadata::generate_json();
-    blueprint_build_utils::build_contracts(vec![
-        "./contracts/lib/eigenlayer-middleware/lib/eigenlayer-contracts",
-        "./contracts/lib/eigenlayer-middleware",
-        "./contracts/lib/forge-std",
-        "./contracts",
-    ]);
+    blueprint_build_utils::build_contracts(vec!["./contracts"]);
 }
