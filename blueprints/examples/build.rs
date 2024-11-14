e94334ae725813a7167f0285659919039ab091dd
Tjemmmic <mdtjemmes@proton.me>
2024-11-13 19:34:33 -0600

    fix(ci): fix build script for ci

--- blueprints/examples/build.rs
+++ blueprints/examples/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/main.rs");
+    println!("cargo:rerun-if-changed=contracts/src/*");
+    blueprint_build_utils::build_contracts(vec!["contracts"]);
     blueprint_metadata::generate_json();
-    blueprint_build_utils::build_contracts(vec!["./contracts"]);
 }
--- blueprints/incredible-squaring-eigenlayer/build.rs
+++ blueprints/incredible-squaring-eigenlayer/build.rs
@@ -5,6 +5,5 @@ fn main() {
         "./contracts/lib/forge-std",
         "./contracts",
     ];
-
     blueprint_build_utils::build_contracts(contract_dirs);
 }
