6b3fd1d140d122a46a158f4569b48556621b261e
Shady Khalifa <dev+github@shadykhalifa.me>
2024-11-27 19:06:39 +0200

    chore: add dummy test for the CI

--- blueprint-manager/src/lib.rs
+++ blueprint-manager/src/lib.rs
@@ -6,3 +6,11 @@ pub mod protocols;
 pub mod sdk;
 pub mod sources;
 pub use executor::run_blueprint_manager;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
