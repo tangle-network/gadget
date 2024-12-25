b9d289d7ea91023c4b98acef6a12c3aaa21b755d
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-12-25 11:38:05 -0500

    chore: fmt

--- crates/macros/blueprint-proc-macro/src/abi/mod.rs
+++ crates/macros/blueprint-proc-macro/src/abi/mod.rs
@@ -1,7 +1,7 @@
+use gadget_std::path::PathBuf;
 use proc_macro::TokenStream;
 use quote::quote;
 use serde_json::Value;
-use gadget_std::path::PathBuf;
 use syn::parse::{Parse, ParseStream};
 use syn::{parse_macro_input, Ident, LitStr, Token};
 
