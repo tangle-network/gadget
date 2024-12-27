415af287ee683eff0d6766041f15af5290dbd6ba
drewstone <drewstone329@gmail.com>
2024-12-28 07:55:44 +0900

    Update sdk/src/lib.rs
    Co-authored-by: Alex <69764315+Serial-ATA@users.noreply.github.com>

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -8,7 +8,6 @@
 
 #![cfg_attr(all(not(feature = "std"), not(feature = "wasm")), no_std)]
 extern crate alloc;
-extern crate core;
 
 /// Benchmark Module
 #[cfg(any(feature = "std", feature = "wasm"))]
