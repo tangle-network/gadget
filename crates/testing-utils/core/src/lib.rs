d4928b2f655008dca7d8af7f3a7863ca54721f5d
Tjemmmic <mdtjemmes@proton.me>
2025-01-03 15:38:10 -0600

    chore(clippy): cleanup, fmt, and fixes

--- crates/testing-utils/core/src/lib.rs
+++ crates/testing-utils/core/src/lib.rs
@@ -1,3 +1,32 @@
+pub use error::TestRunnerError;
+use gadget_config::{ContextConfig, GadgetCLICoreSettings};
+use gadget_logging::info;
+pub use runner::{GenericTestRunner, PlatformConfig, TestJob};
+use std::io::Write;
+use std::path::Path;
+
 mod error;
-pub use error::*;
+pub use error::TestRunnerError as Error;
+
 pub mod runner;
+
+#[allow(irrefutable_let_patterns)]
+pub fn check_for_test(config: &ContextConfig) -> Result<(), Error> {
+    // create a file to denote we have started
+    if let GadgetCLICoreSettings::Run {
+        keystore_uri: base_path,
+        test_mode,
+        ..
+    } = &config.gadget_core_settings
+    {
+        if !*test_mode {
+            return Ok(());
+        }
+        let path = Path::new(base_path).join("test_started.tmp");
+        let mut file = std::fs::File::create(&path)?;
+        file.write_all(b"test_started")?;
+        info!("Successfully wrote test file to {}", path.display())
+    }
+
+    Ok(())
+}
--- crates/testing-utils/tangle/src/lib.rs
+++ crates/testing-utils/tangle/src/lib.rs
@@ -1,3 +1,29 @@
-mod error;
-pub use error::*;
+use gadget_config::{ContextConfig, GadgetCLICoreSettings};
+use gadget_logging::info;
+use std::io::Write;
+use std::path::Path;
+
+pub use gadget_core_testing_utils::Error;
+
 pub mod runner;
+
+#[allow(irrefutable_let_patterns)]
+pub fn check_for_test(config: &ContextConfig) -> Result<(), Error> {
+    // create a file to denote we have started
+    if let GadgetCLICoreSettings::Run {
+        keystore_uri: base_path,
+        test_mode,
+        ..
+    } = &config.gadget_core_settings
+    {
+        if !*test_mode {
+            return Ok(());
+        }
+        let path = Path::new(base_path).join("test_started.tmp");
+        let mut file = std::fs::File::create(&path)?;
+        file.write_all(b"test_started")?;
+        info!("Successfully wrote test file to {}", path.display())
+    }
+
+    Ok(())
+}
