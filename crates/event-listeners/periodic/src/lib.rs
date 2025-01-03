790d1bd4f0f8d55c059ad2d591e6278ba921b0df
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-01-03 15:19:18 -0500

    refactor(event-listeners): flatten crate structures

--- crates/event-listeners/periodic/src/lib.rs
+++ crates/event-listeners/periodic/src/lib.rs
@@ -1,2 +1,42 @@
 pub mod error;
-pub mod periodic;
+use error::Error;
+
+use async_trait::async_trait;
+use gadget_event_listeners_core::{Error as CoreError, EventListener};
+use gadget_std::time::Duration;
+
+#[derive(Default)]
+pub struct PeriodicEventListener<const MSEC: usize, T, Event, Ctx = ()> {
+    listener: T,
+    _pd: gadget_std::marker::PhantomData<(Event, Ctx)>,
+}
+
+#[async_trait]
+impl<
+        const MSEC: usize,
+        T: EventListener<Event, Ctx>,
+        Ctx: Send + Sync + 'static,
+        Event: Send + Sync + 'static,
+    > EventListener<Event, Ctx> for PeriodicEventListener<MSEC, T, Event, Ctx>
+{
+    type ProcessorError = Error;
+
+    async fn new(context: &Ctx) -> Result<Self, CoreError<Self::ProcessorError>>
+    where
+        Self: Sized,
+    {
+        let listener = T::new(context)
+            .await
+            .map_err(|e| Error::InnerListener(e.to_string()))?;
+        Ok(Self {
+            listener,
+            _pd: gadget_std::marker::PhantomData,
+        })
+    }
+
+    async fn next_event(&mut self) -> Option<Event> {
+        let interval = Duration::from_millis(MSEC as u64);
+        tokio::time::sleep(interval).await;
+        self.listener.next_event().await
+    }
+}
