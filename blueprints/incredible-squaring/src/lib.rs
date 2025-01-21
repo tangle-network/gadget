4161b9e0a217b5250e48a39c94af4825a2470861
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-01-21 14:43:50 -0500

    feat: convert tangle incredible squaring to sdk

--- blueprints/incredible-squaring/src/lib.rs
+++ blueprints/incredible-squaring/src/lib.rs
@@ -1,9 +1,11 @@
-use gadget_event_listeners::tangle::events::TangleEventListener;
-use gadget_event_listeners::tangle::services::{services_post_processor, services_pre_processor};
-use gadget_macros::contexts::{ServicesContext, TangleClientContext};
-use gadget_macros::ext::tangle::tangle_subxt::tangle_testnet_runtime::api::services::events::JobCalled;
-use gadget_macros::job;
-use gadget_std::convert::Infallible;
+use std::convert::Infallible;
+use blueprint_sdk::event_listeners::tangle::events::TangleEventListener;
+use blueprint_sdk::event_listeners::tangle::services::{services_post_processor, services_pre_processor};
+use blueprint_sdk::macros::contexts::{ServicesContext, TangleClientContext};
+use blueprint_sdk::macros::ext::tangle::tangle_subxt::tangle_testnet_runtime::api::services::events::JobCalled;
+use blueprint_sdk::macros::job;
+use blueprint_sdk::config::GadgetConfiguration;
+use blueprint_sdk::macros as gadget_macros;
 
 // TODO: Uncomment once tests are working
 #[cfg(test)]
@@ -12,7 +14,7 @@ mod tests;
 #[derive(Clone, TangleClientContext, ServicesContext)]
 pub struct MyContext {
     #[config]
-    pub env: gadget_config::GadgetConfiguration,
+    pub env: GadgetConfiguration,
     #[call_id]
     pub call_id: Option<u64>,
 }
