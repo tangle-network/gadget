9d0ac62296d142f8055e32fb5689b151f6da60cb
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-01-22 15:51:50 -0500

    chore: fmt

--- crates/sdk/src/lib.rs
+++ crates/sdk/src/lib.rs
@@ -63,8 +63,8 @@ pub use gadget_utils as utils;
 pub use gadget_config as config;
 pub use gadget_keystore as keystore;
 pub use gadget_std as std;
-pub use tokio;
 pub use serde;
+pub use tokio;
 
 #[cfg(feature = "local-store")]
 pub use gadget_stores as stores;
