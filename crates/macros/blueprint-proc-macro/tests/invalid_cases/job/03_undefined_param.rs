4d3e866d00244ec75abe785e69b133932bb6a81d
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-01-22 15:50:52 -0500

    fix: get macro tests building

--- crates/macros/blueprint-proc-macro/tests/invalid_cases/job/03_undefined_param.rs
+++ crates/macros/blueprint-proc-macro/tests/invalid_cases/job/03_undefined_param.rs
@@ -1,4 +1,4 @@
-use gadget_blueprint_proc_macro::job;
+use blueprint_sdk::macros::job;
 
 #[job(id = 0, params(n, t), event_listener(listener = null), result(Vec<u8>))]
 fn keygen(n: u16, vec: Vec<u8>) -> Result<Vec<u8>, String> {
