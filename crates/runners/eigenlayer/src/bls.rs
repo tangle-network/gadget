c7d4fac77a7e8085f62fc4052904456ff4c8c612
Donovan Tjemmes <37707055+Tjemmmic@users.noreply.github.com>
2025-01-31 01:27:23 -0600

    fix: remove git dependencies for release (#623)
    * fix(gadget-runners): removed eigensdk git dep
    
    * fix!: switch to tnt-bls and tnt-core-bytecode releases
    
    * fix: mbsm test deployment

--- crates/runners/eigenlayer/src/bls.rs
+++ crates/runners/eigenlayer/src/bls.rs
@@ -128,7 +128,7 @@ async fn register_bls_impl(
     let provider = get_provider_http(&env.http_rpc_endpoint);
 
     let delegation_manager =
-        eigensdk::utils::middleware::delegationmanager::DelegationManager::DelegationManagerInstance::new(
+        eigensdk::utils::core::delegationmanager::DelegationManager::DelegationManagerInstance::new(
             delegation_manager_address,
             provider.clone(),
         );
@@ -183,7 +183,6 @@ async fn register_bls_impl(
     );
 
     let el_writer = ELChainWriter::new(
-        delegation_manager_address,
         strategy_manager_address,
         rewards_coordinator_address,
         el_chain_reader,
