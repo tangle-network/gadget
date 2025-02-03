e20b9707b9ac197d2002bf41431f11ebb1c1ecf1
Donovan Tjemmes <37707055+Tjemmmic@users.noreply.github.com>
2025-02-03 13:21:01 -0600

    feat: blueprint pre-registration (#625)
    * feat!: preregistration for Tangle blueprints
    
    * feat!: configs control preregistration and Eigenlayer support
    
    * fix(ci): workflow debugging
    
    * fix: clippy
    
    * fix: better variable names
    
    * fix: tangle incredible squaring tests
    
    ---------
    
    Co-authored-by: drewstone <drewstone329@gmail.com>

--- crates/testing-utils/tangle/src/node/transactions.rs
+++ crates/testing-utils/tangle/src/node/transactions.rs
@@ -406,20 +406,20 @@ pub async fn setup_operator_and_service<T: Signer<TangleConfig>>(
     sr25519_signer: &T,
     blueprint_id: u64,
     preferences: Preferences,
+    exit_after_registration: bool,
 ) -> Result<u64, TransactionError> {
-    // Join operators
-    join_operators(client, sr25519_signer).await?;
-
-    // Register for blueprint
-    register_blueprint(
-        client,
-        sr25519_signer,
-        blueprint_id,
-        preferences,
-        RegistrationArgs::new(),
-        0,
-    )
-    .await?;
+    if exit_after_registration {
+        // Register for blueprint
+        register_blueprint(
+            client,
+            sr25519_signer,
+            blueprint_id,
+            preferences,
+            RegistrationArgs::new(),
+            0,
+        )
+        .await?;
+    }
 
     // Get the current service ID before requesting new service
     let prev_service_id = get_next_service_id(client).await?;
@@ -460,7 +460,6 @@ pub async fn setup_operator_and_service<T: Signer<TangleConfig>>(
     if service.blueprint != blueprint_id {
         return Err(TransactionError::ServiceIdMismatch);
     }
-
     Ok(new_service_id.saturating_sub(1))
 }
 
