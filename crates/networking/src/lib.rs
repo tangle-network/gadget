96adb44c26e0592abbb9e5f012353f4e0be47b07
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-02-19 15:02:40 -0500

    feat(networking): re-export gadget_crypto::KeyType

--- crates/networking/src/lib.rs
+++ crates/networking/src/lib.rs
@@ -17,6 +17,7 @@ pub use gadget_networking_round_based_extension as round_based_compat;
 
 pub use key_types::*;
 pub use service::{NetworkConfig, NetworkEvent, NetworkService};
+pub use gadget_crypto::KeyType;
 
 #[cfg(all(
     feature = "sp-core-ecdsa",
