51aa9cce05d173eebafa603033da3f7934b35cff
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-02-20 16:57:04 -0500

    chore: cleanup testing leftovers

--- crates/testing-utils/tangle/src/harness.rs
+++ crates/testing-utils/tangle/src/harness.rs
@@ -100,7 +100,7 @@ impl TestHarness for TangleTestHarness {
 
     async fn setup(test_dir: TempDir) -> Result<Self, Self::Error> {
         // Start Local Tangle Node
-        let node = run(NodeConfig::new(true)) // TODO(cleanup): REMOVE
+        let node = run(NodeConfig::new(false))
             .await
             .map_err(|e| Error::Setup(e.to_string()))?;
         let http_endpoint = Url::parse(&format!("http://127.0.0.1:{}", node.ws_port()))?;
